f5d23a009e29e88c19ca44450ad21f69da020116
changgu.han <changgu.han@linecorp.com>
2021-08-22 16:25:37 +0900

    8/22 Record

--- BJ1926.py
+++ BJ1926.py
@@ -1,23 +1,23 @@
 """
 1. 아이디어
-- BFS로 이어진 점들을 확인
-- 한번 지나간 점 표시해서 다시 지나가지 않도록함
+- 2중 for => 값 1 && 방문X => BFS
+- BFS 돌면서 그림 개수 +1, 최대값을 갱신
 
 2. 시간복잡도
 - BFS : O(V+E)
-- V : 500
-- E : 500 * 4
-- V+E : 2500 > 가능
+- V : 500 * 500
+- E : 4 * 500 * 500
+- V+E : 5 * 250000 = 100만 < 2억 >> 가능!
 
 3. 자료구조
-- 전체 지도 : int [][]
-      - 최대 1, int 가능
-- 지나갔는지 여부 : boolean [][]
-
+- 그래프 전체 지도 : int[][]
+- 방문 : bool[][]
+- Queue(BFS)
 """
 
 import sys
 input = sys.stdin.readline
+
 n,m = map(int, input().split())
 map = [list(map(int, input().split())) for _ in range(n)]
 chk = [[False] * m for _ in range(n)]
@@ -25,30 +25,29 @@ chk = [[False] * m for _ in range(n)]
 dy = [0,1,0,-1]
 dx = [1,0,-1,0]
 
-def bfs(y, x) :
-      ecnt = 1
-      q = [(y, x)]
-      while q :
-            ey, ex = q.pop()
-            for k in range(4) :
-                  ny, nx = ey + dy[k], ex + dx[k]
-                  if 0 <= ny < n and 0 <= nx < m :
-                        if map[ny][nx] == 1 and chk[ny][nx] == 0 :
-                              ecnt += 1
-                              chk[ny][nx] = 1
-                              q.append((ny, nx))
-      return ecnt
+def bfs(y, x):
+    rs = 1
+    q = [(y, x)]
+    while q:
+        ey, ex = q.pop()
+        for k in range(4):
+            ny = ey + dy[k]
+            nx = ex + dx[k]
+            if 0<=ny<n and 0<=nx<m:
+                if map[ny][nx] == 1 and chk[ny][nx] == False:
+                    rs += 1
+                    chk[ny][nx] = True
+                    q.append((ny,nx))
+    return rs
 
-cnt, maxv = 0,0
-for j in range(n) :
-      for i in range(m) :
-            if map[j][i] == 1 and chk[j][i] == False :
-                  cnt += 1
-                  chk[j][i] = True
-                  maxv = max(maxv, bfs(j,i))
+cnt = 0
+maxv = 0
+for j in range(n):
+    for i in range(m):
+        if map[j][i] == 1 and chk[j][i] == False:
+            chk[j][i] = True
+            cnt += 1
+            maxv = max(maxv, bfs(j,i))
 
 print(cnt)
 print(maxv)
-
-
-
